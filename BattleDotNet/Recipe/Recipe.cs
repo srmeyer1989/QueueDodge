22f01f329429e295684f3e7badd24769594d750a
srmeyer1989 <srmeyer1989@gmail.com>
2015-12-07 22:19:49 -0600

    Recipe Api added.

---
+++ BattleDotNet/Recipe/Recipe.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleDotNet.Recipe
+{
+
+    public class RootObject
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string profession { get; set; }
+        public string icon { get; set; }
+    }
+}
---
+++ BattleDotNet/Recipe/RecipeEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleDotNet.Recipe
+{
+    public class RecipeEndpoint : IEndpoint
+    {
+        public IList<IEndpointParameter> Parameters { get; set; }
+
+        public RecipeEndpoint(string recipeID, Locale locale, string apiKey)
+        {
+            Parameters = new List<IEndpointParameter>();
+
+            Parameters.Add(new EndpointParameter("recipe", EndpointParameterType.Resource));
+            Parameters.Add(new EndpointParameter("recipeID", EndpointParameterType.Resource));
+            Parameters.Add(new EndpointParameter("locale", locale.ToString(), EndpointParameterType.Query));
+            Parameters.Add(new EndpointParameter("apikey", apiKey, EndpointParameterType.Query));
+
+        }
+    }
+}
