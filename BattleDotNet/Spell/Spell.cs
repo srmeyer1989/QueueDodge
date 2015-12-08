da2c908f3957a96231d63dffbdc7dbba738a798b
srmeyer1989 <srmeyer1989@gmail.com>
2015-12-07 21:55:17 -0600

    Spell Api added.

---
+++ BattleDotNet/Spell/Spell.cs
@@ -0,0 +1,16 @@
+﻿namespace BattleDotNet.Spell
+{
+
+
+    public class Spell
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string icon { get; set; }
+        public string description { get; set; }
+        public string range { get; set; }
+        public string powerCost { get; set; }
+        public string castTime { get; set; }
+        public string cooldown { get; set; }
+    }
+}
---
+++ BattleDotNet/Spell/SpellEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleDotNet.Spell
+{
+    public class SpellEndpoint : IEndpoint
+    {
+        public IList<IEndpointParameter> Parameters { get; set; }
+
+        public SpellEndpoint(string spellID, Locale locale, string apiKey)
+        {
+            Parameters = new List<IEndpointParameter>();
+
+            Parameters.Add(new EndpointParameter("spell", EndpointParameterType.Resource));
+            Parameters.Add(new EndpointParameter("spellID", EndpointParameterType.Resource));
+            Parameters.Add(new EndpointParameter("locale", locale.ToString(), EndpointParameterType.Query));
+            Parameters.Add(new EndpointParameter("apikey", apiKey, EndpointParameterType.Query));
+
+        }
+    }
+}
